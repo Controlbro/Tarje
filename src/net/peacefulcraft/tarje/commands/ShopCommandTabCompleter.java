c2bb97af2c86cb9cebd8f397c7bd56cbe44ceacf
Controlbro <54811794+Controlbro@users.noreply.github.com>
2026-07-31 08:53:27 -0400

    Update ShopCommandTabCompleter.java

--- src/net/peacefulcraft/tarje/commands/ShopCommandTabCompleter.java
+++ src/net/peacefulcraft/tarje/commands/ShopCommandTabCompleter.java
@@ -1,5 +1,6 @@
 package net.peacefulcraft.tarje.commands;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.bukkit.command.Command;
@@ -10,8 +11,11 @@ public class ShopCommandTabCompleter implements TabCompleter {
 
   @Override
   public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-    // TODO Auto-generated method stub
-    return null;
+    if (command.getName().equalsIgnoreCase("sell") && args.length == 1
+        && "hand".startsWith(args[0].toLowerCase())) {
+      return Collections.singletonList("hand");
+    }
+    return Collections.emptyList();
   }
 
 }
